f3a607d52ccd096bd4547ce48ff923c91c2f6ebd
khushi gupta <kg834208@gmail.com>
2025-09-13 01:40:01 +0530

    install toast

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast, { Toaster } from "react-hot-toast";
 
 const pasteSlice = createSlice({
   name: "paste",
@@ -9,9 +10,35 @@ const pasteSlice = createSlice({
   },
   reducers: {
     addToPaste: (state, action) => {
-      state.pastes.push(action.payload);
+      const paste = action.payload;
+      // add a check -> Paste already exist wala case
+      state.pastes.push(paste);
+      localStorage.setItem("pastes", JSON.stringify(state.pastes));
+      toast("Paste Created Sucessfully");
     },
+    UpdateToPaste: (state, action) => {
+      const paste = action.payload;
+      const index = state.pastes.findIndex((item) => item.id === paste.id);
+      if (index >= 0) {
+        state.pastes[index] = paste;
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+        toast.success("Paste updated");
+      }
+    },
+    resetAllPaste: (state) => {
+      state.pastes = [];
 
+      localStorage.removeItem("pastes");
+    },
+    removeFromPaste: (state, action) => {
+      const pasteId = action.payload;
+      const index = state.pastes.findIndex((item) => item.id === pasteId);
+      if (index >= 0) {
+        state.pastes.splice(index, 1);
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+        toast.success("Paste deleted");
+      }
+    },
   },
 });
 export const { addToPaste, UpdateToPaste, resetAllPaste, removeFromPaste } =
